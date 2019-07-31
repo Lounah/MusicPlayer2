a55ee62d05745cc7be68234466aa136bb1bf77f5
lounah <lounahxrussia@gmail.com>
2019-07-31 22:12:37 +0300

    Initial commit

---
+++ app/src/main/java/com/lounah/musicplayer2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lounah.musicplayer2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
